6ecb0df88372ba6fa099046ca5ed9d63c0fb9466
Utsav Patel <utsavpatel@Utsavs-MacBook-Air.local>
2020-10-26 20:11:19 -0700

    Change output dir to /docs for github to find

---
+++ docs/precache-manifest.7a824ea9c5dc2ab4d046f42be35db077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da0961af4523cb07e7e",
+    "url": "/css/app.d1d9e99c.css"
+  },
+  {
+    "revision": "d4de5df5c84cded9f7a752458b774c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be4fb7379ed30c55399d",
+    "url": "/js/about.a0da8c7d.js"
+  },
+  {
+    "revision": "1da0961af4523cb07e7e",
+    "url": "/js/app.e8dcb26f.js"
+  },
+  {
+    "revision": "0b6da5fee4e06f0f246f",
+    "url": "/js/chunk-vendors.37c5aba1.js"
+  },
+  {
+    "revision": "bdfcc3d3089b36a841362405ca88298e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
